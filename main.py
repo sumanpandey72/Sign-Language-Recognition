4e1e6c847319b91a64d4d64e22f02596156159dc
salimjivani <ssalim.jivani@gmail.com>
2021-03-13 15:12:57 -0600

    Added gray live video to tkinter

--- main.py
+++ main.py
@@ -28,39 +28,57 @@ lmain.grid(row=1, column=0)
 
 #start video feed code
 video = cv2.VideoCapture(0)
-static_back = None
+
 upper_left = (350, 50)
 bottom_right = (600, 350)
 
+def gray_live_video():
+    static_back = None
+    while True:
+        _, frame = video.read()
+        frame = cv2.flip(frame, 1)
+        ROI = frame[50:350,350:600]
+        r = cv2.rectangle(frame, upper_left, bottom_right, (100, 50, 200), 1)
+        gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        
+        if static_back is None: 
+            static_back = gray 
+            continue
+
+        diff_ROI = cv2.absdiff(static_back, gray) 
+
+        thresh_ROI = cv2.threshold(diff_ROI, 15, 255, cv2.THRESH_BINARY)[1] 
+        thresh_ROI = cv2.dilate(thresh_ROI, None, iterations = 2) 
+
+        cnts,_ = cv2.findContours(thresh_ROI.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in cnts: 
+            if cv2.contourArea(contour) < 10000: 
+                continue
+            # make rectangle arround the moving object
+            #(x, y, w, h) = cv2.boundingRect(contour)  
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            time.sleep(0.2)
+
+            #save image
+            gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
+            resized = cv2.resize(gray, (28,28), interpolation = cv2.INTER_CUBIC)
+            cv2.imwrite("Output.png",resized)
+
+        return gray
+
 def show_frame():
-    
-    _, frame = video.read()
-    frame = cv2.flip(frame, 1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    img = Image.fromarray(cv2image)
+    img = Image.fromarray(gray_live_video())
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
     lmain.after(10, show_frame) 
-    """
-    #new code
-    check, frame = video.read() 
-    frame = cv2.flip(frame,1)
-    ROI = frame[50:350,350:600]
-    r = cv2.rectangle(frame, upper_left, bottom_right, (100, 50, 200), 1)
-    gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY) 
-
-    img = Image.fromarray(gray)
-    imgtk = ImageTk.PhotoImage(image=img)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    lmain.after(10, show_frame)
-    """
 
 show_frame() 
 
 tk.Label(window,text="Display Results",font=(None, 40)).grid(row=2,column=0)
-tk.Label(window,text="Place Holder for results",font=(None, 40)).grid(row=2,column=0)
+tk.Label(window,text="Place Holder for results",font=(None, 40)).grid(row=3,column=0)
 #canvas.pack()
 
 window.mainloop()  #Starts GUI
